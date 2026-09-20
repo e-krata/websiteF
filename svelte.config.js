bee8af9f98a3eedeb7dbadfcc94321d6f755abde
puspus-dev <boszormenyiadam03@gmail.com>
2026-09-20 10:18:04 +0200

    fix internal error

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess()],
+	preprocess: vitePreprocess(),
 
 	kit: {
 		adapter: adapter(),
 		appDir: '_app'
 	},
+
 	extensions: ['.svelte']
 };
 

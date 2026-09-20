4d12a5c0b2ba63b9909a5738738c00da81a9ce73
puspus-dev <boszormenyiadam03@gmail.com>
2026-09-20 10:24:34 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,16 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+  preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter(),
-		appDir: '_app'
-	},
+  kit: {
+    adapter: adapter(),
+    appDir: '_app'
+  },
 
-	extensions: ['.svelte']
+  extensions: ['.svelte']
 };
 
 export default config;
